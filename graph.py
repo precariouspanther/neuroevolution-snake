5bca7bb99ca8b1a3ec775ed517db16c603285ee9
Adam <adam@foxes.run>
2020-07-25 15:35:44 +1000

    Graph labels

--- graph.py
+++ graph.py
@@ -6,10 +6,18 @@ import matplotlib.backends.backend_agg as agg
 
 class Graph(object):
     def __init__(self):
+        matplotlib.style.use("seaborn")
         matplotlib.use("Agg")
-        fig = plt.figure(figsize=[4, 2])
-        self.ax = fig.add_subplot(111)
-        self.canvas = agg.FigureCanvasAgg(fig)
+        matplotlib.rcParams.update({'font.size': 11})
+        self.fig = plt.figure(figsize=[4, 2])
+
+        self.ax = self.fig.add_subplot(111)
+        self.ax.set_yscale("log")
+        #self.fig.set_facecolor((0, 0, 0))
+        #self.ax.set_facecolor((0.31, 0.31, 0.31))
+
+        self.canvas = agg.FigureCanvasAgg(self.fig)
+        self.fig.tight_layout()
 
     def draw(self, data):
         generations = []
@@ -19,11 +27,15 @@ class Graph(object):
             scores.append(score)
 
         self.ax.clear()
-        self.ax.plot(data)
-        self.canvas.draw()
-        renderer = self.canvas.get_renderer()
 
-        raw_data = renderer.tostring_rgb()
+        self.ax.set_xlabel("Generation", color='k', labelpad=10)
+        self.ax.set_ylabel("Fitness", rotation=270, color='k', labelpad=15)
+        self.ax.set_yscale("log")
+
+        self.ax.plot(data)
+        self.fig.tight_layout()
+        self.canvas.draw()
+        raw_data = self.canvas.get_renderer().tostring_rgb()
         size = self.canvas.get_width_height()
 
         return pygame.image.fromstring(raw_data, size, "RGB")
